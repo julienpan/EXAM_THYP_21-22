ec8233c47880e3d148ccd55b2fd56ced325d0b6a
Julien Pan <julienpan1234@gmail.com>
2021-12-06 11:50:29 +0100

    filtres.html

--- filtres.js
+++ filtres.js
@@ -1,54 +1,30 @@
 var url = "https://polemika.univ-paris8.fr/omk/api/items";
-var ancIndex = -1;
-var ancLigne = "";
-var liste;
 
 
-var url_json = d3
-.json(url, () => {})
-.then((data) => {
-  data.forEach((r) => {
-    // console.log(r);
-    showSujets(r);
-  });
-});
 
-function showSujets(items) {
-    // console.log(items);
-    d3.json(items['@id'], () => {
+function showSujets() {
 
-    }).then((data) => {
+  d3.json(url, () => {}).then((data) => {
+    data.forEach((r) => {
+      d3.json(r['@id'], () => {
+
+      }).then((data) => {
         console.log(data);
-        if(data['dcterms:subject']) {
-            menu[0] += "<ul><li id='item'>" + data['dcterms:subject'][0]['@value'] + "</li></ul>";
-        }
-        d3.select("ul").selectAll('#item').style('cursor', 'pointer').on('click', () => {
-            console.log('CLICKED');
+        d3.select('#menu').append('ul').append('li').style('cursor', 'pointer').on('click', () => {
+          console.log('clicked : ', data)
+          showFiltreItems(data);
+        }).html(() => {
+          return data['dcterms:subject'][0]['@value'];
         })
-    })
-
+      })
+    });
+  });
 }
 
-function showFiltreItems() {
-    // d3.select("ul").selectAll('#item').style('cursor', 'pointer').on('click', () => {
-    //     console.log('CLICKED');
-    // })
+function showFiltreItems(item) {
+  d3.select('#item').append('div').html(() => {
+    return 'Titre : ' + item['o:title'] + '<br>'
+          + 'Identifiant : ' + item['@id'] + '<br>'
+          + 'Media : ' + item['thumbnail_display_urls']['large'];
+  })
 }
-
-var menu = new Array();
-// menu[0] = "<ul><li>1.1</li><li>1.2</li></ul>";
-
-function init() {
-  liste = document.getElementById("menu").getElementsByTagName("li");
-}
-
-function debut(i) {
-  if (ancIndex >= 0) {
-    liste[ancIndex].innerHTML = ancLigne;
-  }
-  ancLigne = liste[i].innerHTML;
-  ancIndex = i;
-  liste[i].innerHTML += menu[i];
-}
-
-
